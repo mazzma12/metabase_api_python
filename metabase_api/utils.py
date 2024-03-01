7b7b350dffc025b99a6c6d1885b44d26837c6300
mazzma12 <matthieu@crowdsec.net>
2024-03-01 10:38:12 +0100

    fixup! Add utils.py and gitignore (#3)

--- metabase_api/utils.py
+++ metabase_api/utils.py
@@ -3,9 +3,13 @@ import uuid
 def format_variable_param(key, value, type_="category"):
     return [
         {
-            "id": uuid.uuid4(),
+            "id": str(uuid.uuid4()),
             "type": type_,
             "value": value,
             "target": ["variable", ["template-tag", key]],
         }
     ]
+
+if __name__ == "__main__":
+    res = format_variable_param("organization", "1234")
+    print(res)
